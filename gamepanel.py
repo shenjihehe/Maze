d0fa97f47e26708bb0f5d95afcd59ca6ab409fbb
Michael Lin <linzichunzf@hotmail.com>
2016-03-27 19:26:53 -0600

    init repo

---
+++ gamepanel.py
@@ -0,0 +1,341 @@
+import time
+import math
+import tkinter
+from tkinter import *
+from threading import Timer
+
+
+from maze import *
+
+
+
+
+class Application(Frame):
+    """
+    GUI, implement fcuntion from maze.py to generate maze
+    """
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack(expand=True, fill=BOTH)
+        self.master = master
+        self.maze = Maze(40, 40) # maze object
+        self.createWindow()
+        self.initCvs()
+        self.initButton()
+        self.show_icon = False
+
+    def createWindow(self):
+        """
+        Set windows size, and central it
+        :return:
+        """
+        sw = self.master.winfo_screenwidth()
+        sh = self.master.winfo_screenheight()
+
+        self.h = sh - 100
+        self.w = self.h
+
+        self.block_size = int((self.h - 40) / (max(self.maze.row_size, self.maze.col_size)))
+        self.hero_x = self.maze.start_p[0]
+        self.hero_y = self.maze.start_p[1]
+        self.hero_id = 0
+
+        self.master.title("card game")
+        # central
+        x = (sw - self.w) / 2
+        y = (sh - self.h) / 2
+        # width * length + x-cord + y-cord
+        self.master.geometry('%dx%d+%d+%d' % (self.w, self.h, x, y))
+
+    def initButton(self):
+        self.bottomFrame = Frame(self, borderwidth=2, relief="groove")
+
+        self.bottomFrame.pack(side=BOTTOM, expand=FALSE, fill=BOTH)
+
+        # col size
+        Label(self.bottomFrame, text="col size:").pack(side=LEFT, pady=3, expand=True)
+        # default value, unless user specific at GUI
+        self.row_num = IntVar(value=40)
+        self.row_num._default = 40
+
+        size = Entry(self.bottomFrame, textvariable=self.row_num, width=6)
+        size.tk_focusNext()
+        size.pack(side=LEFT, pady=3, expand=True)
+
+
+        # row size
+        Label(self.bottomFrame, text="row size:").pack(side=LEFT, pady=3, expand=True)
+        # default value, unless user specific at GUI
+        self.col_num = IntVar(value=40)
+        self.col_num._default = 40
+
+        size = Entry(self.bottomFrame, textvariable=self.col_num, width=6)
+        size.pack(side=LEFT, pady=3, expand=True)
+
+        # user can config how long with the icon path last for
+        Label(self.bottomFrame, text="icon show time:").pack(side=LEFT, pady=3, expand=True)
+        # default value, unless user speicif
+        self.icon_time = IntVar(value=8)
+        self.icon_time._default = 8
+
+        size = Entry(self.bottomFrame, textvariable=self.icon_time, width=6)
+        size.pack(side=LEFT, pady=3, expand=True)
+        # 'New Game' and 'Quit Game' buttom
+        newGameButton = Button(self.bottomFrame, text="New Game",
+                               command=self.handler.on_new_game)
+        newGameButton.pack(side=LEFT, pady=3, expand=True)
+
+        quitGameButton = Button(self.bottomFrame, text="Quit Game",
+                                command=self.handler.on_quit_game)
+        quitGameButton.pack(side=LEFT, pady=3, expand=True)
+
+    def initCvs(self):
+        # create top frame, main canvane, and control event
+        self.topFrame = Frame(self, background="green")
+        self.mainCvs = Canvas(self.topFrame)
+        self.handler = Handler(self)
+        self.mainCvs.bind('<Up>', self.handler.move) # press 'UP'
+        self.mainCvs.bind('<Down>', self.handler.move) # press 'DOWN'
+        self.mainCvs.bind('<Left>', self.handler.move) # press 'LEFT'
+        self.mainCvs.bind('<Right>', self.handler.move) # press 'RIGHT'
+        self.mainCvs.bind('<Button-1>', self.handler.onclick)
+        self.mainCvs.pack(expand=True, fill=BOTH)
+        self.topFrame.pack(fill=BOTH, expand=True, side=TOP)
+
+    def light_block(self, i, j):
+        """
+        determine what area shoule be blocked to player
+        """
+        # only within two blocks is avaiable to player
+        if math.fabs(self.hero_y - j) > 2 or math.fabs(self.hero_x - i) > 2:
+            return False
+        # hero is avaibale
+        if (i, j) == (self.hero_x, self.hero_y):
+            return True
+        # 对某点上下左右四个方向判断,如果四个方向有一个是英雄的位置 是 白
+        # 否则,在对这个各个方向的 各个点进行相同判断
+
+        # 也就是,如果某点可以在2步之内走到英雄处,为白
+        for a in range(4):
+            (npx, npy) = self.maze.get_arround(i, j, a, 1)
+            if npx:
+                if self.maze.maze_map[npx][npy] == 1:
+                    continue
+                if (npx, npy) == (self.hero_x, self.hero_y):
+                    return True
+                for b in range(4):
+                    (nnpx, nnpy) = self.maze.get_arround(npx, npy, b, 1)
+                    if nnpx:
+                        if self.maze.maze_map[nnpx][nnpy] == 1:
+                            continue
+                        if nnpx and (nnpx, nnpy) == (self.hero_x, self.hero_y):
+                            return True
+
+    def init_blocks(self):
+        self.color_map = {}
+        for i in range(self.maze.row_size):
+            for j in range(self.maze.col_size):
+                type = self.maze.maze_map[i][j]
+                if type == 0:
+                    self.color_map[(i, j)] = self.draw_block(i, j, "gray")
+                if type == 1:
+                    self.color_map[(i, j)] = self.draw_block(i, j, "black")
+        # drwa player
+        self.draw_hero()
+        # draw exit
+        self.draw_block(self.maze.end_p[0], self.maze.end_p[1], "green")
+        # draw bonue - coin path
+        self.award_id = ''
+        self.draw_award()
+
+    def draw_hero(self):
+        """
+        when player position changes, redraw canvas
+        """
+        # remove coin path
+        self.mainCvs.delete("coin")
+        # delete prev_player
+        self.mainCvs.delete(self.hero_id)
+        # draw player
+        self.hero_id = self.mainCvs.create_rectangle((self.hero_x + (1 / 4)) * self.block_size,
+                                                     (self.hero_y + (1 / 4)) * self.block_size,
+                                                     (self.hero_x + (3 / 4)) * self.block_size,
+                                                     (self.hero_y + (3 / 4)) * self.block_size,
+                                                     fill="blue", outline="blue")
+        # check every position, you may comment out this part
+        for i in range(self.maze.row_size):
+            for j in range(self.maze.col_size):
+                id = self.color_map.get((i, j))
+                type = self.maze.maze_map[i][j]
+                if self.light_block(i, j):
+                    # yes!!
+                    if type == 0 and self.mainCvs.itemcget(id, "fill") != "gray":
+                        self.mainCvs.itemconfigure(id, fill="gray", outline="gray")
+                    # draw coin
+                    elif type == 7 and self.show_icon:
+                        c_id = self.draw_coin(i, j, "gold")
+                        self.mainCvs.itemconfigure(c_id, tags="coin")
+                        self.mainCvs.itemconfigure(id, fill="gray", outline="gray")
+                else:
+                    if type == 7 and self.show_icon:
+                        c_id = self.draw_coin(i, j, "gold")
+                        self.mainCvs.itemconfigure(c_id, tags="coin")
+                    elif self.mainCvs.itemcget(id, "fill") != "black":
+                        self.mainCvs.itemconfigure(id, fill="black", outline="black")
+
+    def draw_coin(self, i, j, color):
+        """
+        at coordinate(i, j), draw coin(circle)
+        """
+        return self.mainCvs.create_oval((i + (1 / 4)) * self.block_size,
+                                        (j + (1 / 4)) * self.block_size,
+                                        (i + (3 / 4)) * self.block_size,
+                                        (j + (3 / 4)) * self.block_size,
+                                        fill=color, outline=color)
+
+    def draw_block(self, i, j, color):
+        """
+        at coordinate(i, j), draw block with 'color'
+        """
+        return self.mainCvs.create_rectangle((i) * self.block_size, (j) * self.block_size,
+                                             (i + 1) * self.block_size, (j + 1) * self.block_size,
+                                             fill=color, outline=color)
+
+    def draw_award(self):
+        """
+        draw a red block as bonus
+        randomly generate one
+        only one upon a time
+        :return:
+        """
+        if self.award_id:
+            self.mainCvs.delete(self.award_id)
+
+        while True:
+            self.award_p = self.maze.random_point(self.maze.row_size, self.maze.col_size)
+            if self.maze.maze_map[self.award_p[0]][self.award_p[1]] == 0:
+                break
+        self.award_id = self.draw_block(*self.award_p, "red") # draw red block
+
+    def set_show_icon(self, bool):
+        """
+        implement timer, determine when to get rid of 'coin path'
+        """
+        self.show_icon = bool
+        if bool == False:
+            for i in range(self.maze.row_size):
+                for j in range(self.maze.col_size):
+                    if self.maze.maze_map[i][j] == 7:
+                        self.maze.maze_map[i][j] = 0
+            self.draw_hero()
+
+    def show_message(self, m):
+        """
+        popup message
+
+
+        """
+        import tkinter.messagebox
+        # default messagebox
+        tkinter.messagebox.showinfo(title="Note", message=m)
+
+
+
+
+
+
+
+class Handler(object):
+    """
+    event handler
+    """
+
+    def __init__(self, app):
+        self.app = app
+        self.maze = app.maze
+        self.mainCvs = app.mainCvs
+
+    def onclick(self, event):
+        """
+        handling
+        click event
+
+        """
+        self.mainCvs.focus_force()
+
+    def on_new_game(self):
+        """
+        create a new game
+        """
+        self.mainCvs.focus_force()
+        self.mainCvs.delete('all')
+        if self.app.show_icon == True:
+            self.timer.cancel()
+        if self.app.row_num.get() < 1 :
+            self.app.row_num.__init__()
+        if self.app.col_num.get() < 1 or self.app.col_num.get() > 40:
+            self.app.col_num.__init__()
+        self.app.hero_id = 0
+        self.maze.__init__(self.app.row_num.get(), self.app.col_num.get())
+        self.app.hero_x = self.maze.start_p[0]
+        self.app.hero_y = self.maze.start_p[1]
+        self.app.init_blocks() # start drawing block
+
+    def on_quit_game(self): # when 'Quit Game'
+        if self.app.show_icon == True:
+            self.timer.cancel()
+        self.app.master.quit() # quit
+
+    def on_draw_path(self):
+        """
+        slove 'click'
+        """
+        self.app.show_message("not devlop") # not avaibale for now
+
+    def move(self, event):
+        """
+        handling keyboard
+        """
+        key_name = event.keysym
+        cx = self.app.hero_x
+        cy = self.app.hero_y
+        if key_name == "Up": # UP
+            cy = self.app.hero_y - 1
+        if key_name == "Down": # DOWN
+            cy = self.app.hero_y + 1
+        if key_name == "Left": # LEFT
+            cx = self.app.hero_x - 1
+        if key_name == "Right": # RIGHT
+            cx = self.app.hero_x + 1
+        # when reach exit
+        if (cx, cy) == self.maze.end_p:
+            self.app.show_message("you win the game!")
+            self.on_new_game()
+        if self.maze.maze_map[cx][cy] == 0:
+            # when player reach bonus - red block - coin path
+            if (cx, cy) == self.app.award_p:
+                self.maze.solve((cx, cy))
+                # redraw bonus - red block
+                self.app.draw_award()
+                # set how long with it last for
+                self.app.set_show_icon(True)
+                self.timer = Timer(self.app.icon_time.get(), self.app.set_show_icon, (False,))
+                self.timer.start()
+            self.app.hero_x = cx
+            self.app.hero_y = cy
+            self.app.draw_hero()
+        # if player lay on a coin
+        if self.maze.maze_map[cx][cy] == 7:
+            # get rid of that one
+            self.maze.maze_map[cx][cy] = 0
+
+            self.app.hero_x = cx
+            self.app.hero_y = cy
+            self.app.draw_hero()
+
+# main function, start GUI
+if __name__ == "__main__":
+    root = Tk()
+    app = Application(master=root)
+    app.mainloop()
---
+++ maze.py
@@ -0,0 +1,205 @@
+﻿import random
+import math
+import collections
+
+
+class Maze(object):
+    """
+    Create the maze, and also the solution
+    """
+
+    def __init__(self, row=41, col=41):
+        # make sure its odd number
+        self.row_size = int(row / 2) * 2 + 1
+        self.col_size = int(col / 2) * 2 + 1
+
+        self.maze_map = [[0 for x in range(self.col_size)] for y in range(self.row_size)]
+
+        self.init_map()
+        self.make_row()
+        (self.start_p, self.end_p) = self.start_end_point()
+
+    def display(self):
+        """
+        You can print a maze at terminal
+        :return:
+        """
+        for i in self.maze_map:
+            print(i)
+
+    def init_map(self):
+        """
+        create a m*m list(array) as maze
+        :return:
+        """
+        for row in range(self.col_size):
+            for col in range(self.row_size):
+                if row % 2 == 0 or col % 2 == 0:
+                    self.maze_map[col][row] = 1
+
+    def make_row(self):
+        """
+        implement dfs to create the maze
+        """
+        stack = []
+        self.visted = []
+        rp = self.random_point(self.col_size, self.row_size)
+        stack.append(((1,1)))
+        while stack:
+            cp = stack.pop()
+            # create more interesection, make the game harder
+            if random.random() < 0.8:
+                self.visted.append(cp)
+            np = self.get_next_point(cp)
+            if np:
+                stack.append(cp)
+                stack.append(np)
+
+    def get_next_point(self, p):
+        """
+        get a random neighbout of point 'p'
+        """
+        x, y = p[0], p[1]
+        directions = list(range(4))
+        random.shuffle(directions)
+        for i in directions:
+            # the point after the next-point
+            (a, b) = self.get_arround(x, y, i, 2)
+            if not a or a >= self.row_size or a <= 0 or b >= self.col_size or b <= 0:  # if valid
+                continue
+            if (a, b) in self.visted:  # if visited
+                continue
+            # if satisfied the both condition, create a path, and return it
+            (c, d) = self.get_arround(x, y, i, 1)
+            self.maze_map[c][d] = 0
+            return (a, b) # return
+        return None
+
+    def get_arround(self, x, y, direction, juli=1):
+        """
+        get point, at (x,y) from 'direction'
+        """
+        a, b = 0, 0
+        if direction == 0:
+            a, b = x, y - juli
+        elif direction == 1:
+            a, b = x + juli, y
+        elif direction == 2:
+            a, b = x, y + juli
+        elif direction == 3:
+            a, b = x - juli, y
+        if a >= self.row_size or a <= 0 or b >= self.col_size or b <= 0:  # 房间号在许可范围内
+            return None, None
+        else:
+            return (a, b)
+
+    def adj(self, x, y):
+        """
+        get adj(not wall)
+        """
+        ls = []
+        for d in range(4):
+            np = self.get_arround(x, y, d, 1) # get point at (x,y) from 'd'
+            if np[0] is None:
+                continue
+            elif self.maze_map[np[0]][np[1]] != 1:
+                ls.append(np)
+        return ls
+
+    def random_point(self, row, col):
+        """
+        generate a point between 'row' and 'col', coulbe be either path or wall
+        """
+        return (random.randint(0, row - 1), random.randint(0, col - 1))
+
+    def start_end_point(self):
+        """
+        create entrance and exit
+        :return:
+        """
+        while True:
+            while True:
+                start_p = self.random_point(self.row_size, self.col_size)
+                if self.maze_map[start_p[0]][start_p[1]] == 0:
+                    break
+            while True:
+                end_p = self.random_point(self.row_size, self.col_size)
+                if self.maze_map[end_p[0]][end_p[1]] == 0:
+                    break
+            dy = math.fabs(start_p[1] - end_p[1])
+            dx = math.fabs(start_p[0] - end_p[0])
+            if dx + dy > (self.col_size + self.row_size) / 2:
+                return (start_p, end_p)
+
+    # def solve_bf(self, start_point):
+    #     """
+    #     solve a path from certain point to exit
+    #     """
+    #
+    #     def get_next_access_point(p, visited):
+    #         x, y = p[0], p[1]
+    #         min_w = None
+    #         min_p = None
+    #         for d in range(0, 4):
+    #             (a, b) = self.get_arround(x, y, d, 1)
+    #
+    #             if not a or visited and (a, b) in visited:
+    #                 continue
+    #             if self.maze_map[a][b] == 1:
+    #                 continue
+    #             if min_w:
+    #                 if math.fabs(a - self.end_p[0]) + math.fabs(b - self.end_p[1]) < min_w:
+    #                     min_p = (a, b)
+    #                     min_w = math.fabs(a - self.end_p[0]) + math.fabs(b - self.end_p[1])
+    #             else:
+    #                 min_w = math.fabs(a - self.end_p[0]) + math.fabs(b - self.end_p[1])
+    #                 min_p = (a, b)
+    #         return min_p
+    #
+    #     visted = []
+    #     stack = [] # path
+    #     stack.append(start_point)
+    #     while len(stack) != 0:
+    #         cp = stack.pop()
+    #         if cp == self.end_p:
+    #             break
+    #         visted.append(cp)
+    #         np = get_next_access_point(cp, visted)
+    #         if np is None or np in visted:
+    #             continue
+    #         else:
+    #             stack.append(cp)
+    #             stack.append(np)
+    #     return stack # return the path
+
+    def solve(self, start_point):
+        '''Solve a path from certain point to exit
+        for 'coint path' - red block - bonus
+        '''
+        queue = []
+        father = {}
+        visted = []
+        queue.append(start_point)
+        while True:
+            cp = queue.pop(0)
+            visted.append(cp)
+            if cp == self.end_p:
+                break
+            for np in self.adj(*cp):
+                if np and np not in visted:
+                    queue.append(np)
+                    father[np] = cp
+        cp = self.end_p
+        for i in range(len(father)):
+            fp = father.get(cp)
+            if fp == self.start_p:
+                break
+            else:
+                cp = fp
+                if cp:
+                    self.maze_map[cp[0]][cp[1]] = 7
+
+if __name__ == "__main__":
+    m = Maze(20, 20)
+    # path = m.solve_bf(m.start_p)
+    m.display()
